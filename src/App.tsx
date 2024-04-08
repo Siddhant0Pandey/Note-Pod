c6599d585d593b15823b789856ebfe4da35511af
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-04-08 15:07:04 +0545

    file setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
