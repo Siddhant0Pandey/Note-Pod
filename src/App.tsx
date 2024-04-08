163b9cf733c28853838d1e902cf129085c0f6948
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-04-08 19:03:50 +0545

    feat: Input feature updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./Components/NewNote";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<h1>Hello</h1>}></Route>
-      </Routes>
+      <Container className="my-4">
+        <Routes>
+          <Route path="/" element={<h1>Hello</h1>}></Route>
+          <Route path="new" element={<NewNote />}></Route>
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/:id">
+            <Route index element={<h1>This is show</h1>} />
+            <Route path="edit" element={<h1>This is edit</h1>} />
+          </Route>
+        </Routes>
+      </Container>
     </>
   );
 }
